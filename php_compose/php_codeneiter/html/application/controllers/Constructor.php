1393b340fe5d3c0ad4ac84f1038d078b00253ea2
benjamin-sudo <78278324+benjamin-sudo@users.noreply.github.com>
2023-12-19 18:55:43 -0300

    xd

--- php_compose/php_codeneiter/html/application/controllers/Constructor.php
+++ php_compose/php_codeneiter/html/application/controllers/Constructor.php
@@ -5,25 +5,21 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Constructor extends CI_Controller {
 
     public function index()  {
-        
         //echo "<br>";
-
         //$host       =   $_SERVER['HTTP_HOST'];
         //$uri        =   rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
         //echo $base  =   "http://" . $host . $uri . "/";
-        
+        log_message('error', 'USER_INFO ' . 'date');
         $this->load->view('inicio');
     }
 
     public function login(){
         //if (!$this->input->is_ajax_request()) {  show_404(); }
-    
 
         $this->output->set_output(json_encode([
             'status' => true
         ]));
     }
-
 }
 ?>
 
