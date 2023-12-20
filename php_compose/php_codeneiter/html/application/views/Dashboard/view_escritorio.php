9202ff19a1713f42ad1da4fef1c58878910f2ee6
benjamin-sudo <78278324+benjamin-sudo@users.noreply.github.com>
2023-12-20 16:58:33 -0300

    xd

--- php_compose/php_codeneiter/html/application/views/Dashboard/view_escritorio.php
+++ php_compose/php_codeneiter/html/application/views/Dashboard/view_escritorio.php
@@ -14,6 +14,7 @@
 <body class="hold-transition sidebar-mini">
 <!-- Site wrapper -->
 <div class="wrapper">
+    
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
@@ -53,8 +54,6 @@
           </form>
         </div>
       </li>
-
-    
     </ul>
   </nav>
   <!-- /.navbar -->
@@ -66,7 +65,7 @@
       <img src="assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Clinica Libre</span>
     </a>
-
+    
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user (optional) -->
@@ -82,219 +81,85 @@
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <li class="nav-header">NOMBRE DEL MENU</li>
+            <li class="nav-header">NOMBRE DEL MENU</li>
             <li class="nav-item">
                 <a href="#" class="nav-link">
                     <i class="nav-icon fas fa-book"></i>
-                    <p>Pages<i class="fas fa-angle-left right"></i></p>
+                    <p>BDU paciente<i class="fas fa-angle-left right"></i></p>
                 </a>
-
                 <ul class="nav nav-treeview">
-                    <li class="nav-item">
-                        <a href="../examples/invoice.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Invoice</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../examples/profile.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Profile</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../examples/e-commerce.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>E-commerce</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../examples/projects.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Projects</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../examples/project-add.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Project Add</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../examples/project-edit.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Project Edit</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../examples/project-detail.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Project Detail</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../examples/contacts.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Contacts</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../examples/faq.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>FAQ</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../examples/contact-us.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Contact us</p>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-
-
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon far fa-plus-square"></i>
-              <p>  Extras   <i class="fas fa-angle-left right"></i>  </p>
-            </a>
-
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p> sub menu 1
-                    <i class="fas fa-angle-left right"></i>
-                  </p>
-                </a>
-
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="../examples/login.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Login v1</p>
+                <li class="nav-item">
+                    <a href="#" class="nav-link"><i class="far fa-circle nav-icon"></i>
+                        <p>Maestro de pacientes<i class="fas fa-angle-left right"></i></p>
                     </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/register.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Register v1</p>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link"><p>BDU</p></a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p> Login & Register v2 <i class="fas fa-angle-left right"></i> </p>
                     </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/forgot-password.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Forgot Password v1</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/recover-password.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Recover Password v1</p>
-                    </a>
-                  </li>
-                </ul>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="../examples/login-v2.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Login v2</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../examples/register-v2.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Register v2</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../examples/forgot-password-v2.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Forgot Password v2</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../examples/recover-password-v2.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Recover Password v2</p>
+                            </a>
+                        </li>
+                    </ul>
               </li>
 
-
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>
-                    Login & Register v2
-                    <i class="fas fa-angle-left right"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="../examples/login-v2.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Login v2</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/register-v2.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Register v2</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/forgot-password-v2.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Forgot Password v2</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/recover-password-v2.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Recover Password v2</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-
-
-              
-         
-
-         
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
+
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Blank Page</h1>
+            <h1>Pagina Bienvenida</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
+              <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+              <li class="breadcrumb-item active">Pagina inicio</li>
             </ol>
           </div>
         </div>
       </div><!-- /.container-fluid -->
     </section>
 
-    <!-- Main content -->
-    <section class="content">
-
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Title</h3>
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
-        </div>
-        <div class="card-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
-      </div>
-      <!-- /.card -->
-
-    </section>
+  
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
@@ -303,7 +168,7 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.2.0
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Clinica libre &copy; <?php echo date('d-m-Y'); ?> <a href="https://adminlte.io">#</a>.</strong> Todos los derechos reservados
   </footer>
 
   <!-- Control Sidebar -->
