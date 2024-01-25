0f3755d2bc50ca0a35ca154e130410827f2d7d0a
benjamin-sudo <78278324+benjamin-sudo@users.noreply.github.com>
2024-01-25 12:24:12 -0300

    xd

--- php_compose/php_codeneiter/html/assets/ssan_pres_agregaeditaprestador/js/javascript.js
+++ php_compose/php_codeneiter/html/assets/ssan_pres_agregaeditaprestador/js/javascript.js
@@ -105,7 +105,7 @@ function CARGAPROF(_value){
                                                                         select.appendChild(option);
                                                                     });
                                                                 } 
-                                                                
+                                                                $('#loadFade').modal('hide');
                                                             }, 
             complete    :   function()                      {
                                                                 $('#loadFade').modal('hide');
