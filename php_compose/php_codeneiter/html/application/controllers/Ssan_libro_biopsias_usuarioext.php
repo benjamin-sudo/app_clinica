3802d2ff8ee4d61639b5523098dc49245ab1108d
benjamin-sudo <78278324+benjamin-sudo@users.noreply.github.com>
2024-01-25 16:46:39 -0300

    XD

--- php_compose/php_codeneiter/html/application/controllers/Ssan_libro_biopsias_usuarioext.php
+++ php_compose/php_codeneiter/html/application/controllers/Ssan_libro_biopsias_usuarioext.php
@@ -5,25 +5,14 @@ class Ssan_libro_biopsias_usuarioext extends CI_Controller {
     function __construct() {
         parent::__construct();
         $this->load->library('session');
-        $this->load->model("Ssan_libro_biopsias_usuarioext_model");
+        //$this->load->model("Ssan_libro_biopsias_usuarioext_model");
     }
     
     public function index(){
         $this->output->set_template('blank');
         $data       =   [];
-        $this->load->css("assets/Ssan_libro_biopsias_usuarioext/css/styles.css");
-        $this->load->js("assets/Ssan_libro_biopsias_usuarioext/js/javascript.js");
+        #$this->load->css("assets/Ssan_libro_biopsias_usuarioext/css/styles.css");
+        #$this->load->js("assets/Ssan_libro_biopsias_usuarioext/js/javascript.js");
         $this->load->view('Ssan_libro_biopsias_usuarioext/Ssan_libro_biopsias_usuarioext_view',$data);
-        
-
-        /*
-        $data       =   [];
-        $this->load->css("assets/Ssan_hdial_agendadialisis/css/styles.css");
-        $this->load->js("assets/Ssan_hdial_agendadialisis/js/javascript.js");
-        $this->load->view('Ssan_hdial_agendadialisis/Ssan_hdial_agendadialisis_view',$data);
-        */
     }
-
-
-
 }
---
+++ php_compose/php_codeneiter/html/application/views/ssan_libro_biopsias_usuarioext/ssan_libro_biopsias_usuarioext_view.php
@@ -0,0 +1,3 @@
+<h4 class="title" style="color:#e34f49;margin:0px;margin-left:12px;">
+    <i class="nav-icon fa fa-tint" aria-hidden="true"></i>&nbsp;<b>NUEVA SOLICITUD DE ANATOMIA PATOLOGICA</b>
+</h4>
