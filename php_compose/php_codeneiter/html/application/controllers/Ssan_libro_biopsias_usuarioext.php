8b32653017d78e1829a5ee872ad312895fa318e3
benjamin-sudo <78278324+benjamin-sudo@users.noreply.github.com>
2024-01-25 16:50:32 -0300

    xd

--- php_compose/php_codeneiter/html/application/controllers/Ssan_libro_biopsias_usuarioext.php
+++ php_compose/php_codeneiter/html/application/controllers/Ssan_libro_biopsias_usuarioext.php
@@ -11,8 +11,8 @@ class Ssan_libro_biopsias_usuarioext extends CI_Controller {
     public function index(){
         $this->output->set_template('blank');
         $data       =   [];
-        #$this->load->css("assets/Ssan_libro_biopsias_usuarioext/css/styles.css");
-        #$this->load->js("assets/Ssan_libro_biopsias_usuarioext/js/javascript.js");
+        $this->load->css("assets/Ssan_libro_biopsias_usuarioext/css/styles.css");
+        $this->load->js("assets/Ssan_libro_biopsias_usuarioext/js/javascript.js");
         $this->load->view('Ssan_libro_biopsias_usuarioext/Ssan_libro_biopsias_usuarioext_view',$data);
     }
 }
--- php_compose/php_codeneiter/html/assets/ssan_libro_biopsias_usuarioext/js/javascript.js
+++
@@ -1,6 +0,0 @@
-//Contenido JS de la Extensión ssan_libro_biopsias_usuarioext.
-
-$(function () {
-
-   console.log("SYSDATE"); 
-});
