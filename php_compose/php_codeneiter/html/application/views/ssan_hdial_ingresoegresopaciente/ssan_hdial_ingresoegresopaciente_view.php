4139b84c1eb85cb9e6b1e3096d2fe313cbec16f1
benjamin-sudo <78278324+benjamin-sudo@users.noreply.github.com>
2024-02-17 17:01:07 -0300

    17.02.2024

--- php_compose/php_codeneiter/html/application/views/ssan_hdial_ingresoegresopaciente/ssan_hdial_ingresoegresopaciente_view.php
+++ php_compose/php_codeneiter/html/application/views/ssan_hdial_ingresoegresopaciente/ssan_hdial_ingresoegresopaciente_view.php
@@ -1,3 +1,4 @@
 <h4 class="title" style="color:#e34f49;margin:0px;margin-left:12px;">
     <i class="nav-icon fa fa-tint" aria-hidden="true"></i>&nbsp;<b>INGRESO DE PACIENTES Y TURNOS EN DIALISIS</b>
 </h4>
+
