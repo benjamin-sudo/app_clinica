f58f56fb980d9934e9603862f1ea605a48a4c020
benjamin-sudo <78278324+benjamin-sudo@users.noreply.github.com>
2024-02-17 19:28:13 -0300

    xd

--- php_compose/php_codeneiter/html/application/config/config.php
+++ php_compose/php_codeneiter/html/application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:9015/';
-
+#$config['base_url'] = 'http://localhost:9015/';
+$config['base_url'] = 'http://192.168.1.5:9015/';
 /*
 |--------------------------------------------------------------------------
 | Index File
