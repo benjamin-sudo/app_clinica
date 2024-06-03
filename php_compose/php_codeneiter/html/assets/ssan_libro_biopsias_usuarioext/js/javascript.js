0c28cdeac9972408803b9add1ecac8d2a7c8dac0
benjamin-sudo <78278324+benjamin-sudo@users.noreply.github.com>
2024-06-03 15:50:03 -0400

    03.06.2024

--- php_compose/php_codeneiter/html/assets/ssan_libro_biopsias_usuarioext/js/javascript.js
+++ php_compose/php_codeneiter/html/assets/ssan_libro_biopsias_usuarioext/js/javascript.js
@@ -58,7 +58,7 @@ $(document).ready(function(){
     $('#Dv_verdocumentos').on('hidden.bs.modal',function(e){ 
         $("#PDF_VERDOC").html(''); 
     });
-    ACTUALIZA_FECHA_ANATOMIAPATOLOGICA(2);
+    //ACTUALIZA_FECHA_ANATOMIAPATOLOGICA(2);
 });
 
 function nueva_solicitud_anatomia(NUM_FICHAE,ADMISION){
