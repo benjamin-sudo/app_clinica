a6a84860fee44e730e0fc35f9afdc87e45b80bc3
benjamin-sudo <78278324+benjamin-sudo@users.noreply.github.com>
2024-08-15 23:54:26 -0400

    zs

--- php_compose/backup_service/backup.sh
+++ php_compose/backup_service/backup.sh
@@ -8,5 +8,11 @@ BACKUP_DIR="/backup_service/backup"
 BACKUP_FILE="$BACKUP_DIR/mysql_backup_$DATE.tar.gz"
 # Crear respaldo del volumen
 tar czvf $BACKUP_FILE /var/lib/mysql
-# Opcional: Eliminar respaldos más antiguos de 7 días
+# Eliminar respaldos más antiguos de 7 días
 find $BACKUP_DIR -type f -name "*.tar.gz" -mtime +7 -exec rm {} \;
+# Eliminar el archivo más antiguo si ya existen más de 10 archivos de respaldo
+NUM_FILES=$(ls -1 $BACKUP_DIR/*.tar.gz | wc -l)
+if [ $NUM_FILES -gt 10 ]; then
+    OLDEST_FILE=$(ls -1t $BACKUP_DIR/*.tar.gz | tail -1)
+    rm $OLDEST_FILE
+fi
