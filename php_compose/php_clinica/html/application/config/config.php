7031b3ddf2349c2e30b89c440dc9ce136cf2f26b
benjamin-sudo <78278324+benjamin-sudo@users.noreply.github.com>
2024-08-16 13:26:24 -0400

    16.08.2024

--- php_compose/php_clinica/html/application/config/config.php
+++ php_compose/php_clinica/html/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:9025/';
+$config['base_url'] = getenv('APP_BASE_URL') ?: 'https://www.clinicalibre.cl';
 #$config['base_url'] = 'http://192.168.1.5:9015/';
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +36,6 @@ $config['base_url'] = 'http://localhost:9025/';
 |
 */
 $config['index_page'] = '';
-
 /*
 |--------------------------------------------------------------------------
 | URI PROTOCOL
