e0b3bcf764d8842ba135a2f96186f44818b7c944
benjamin-sudo <78278324+benjamin-sudo@users.noreply.github.com>
2024-08-18 15:20:19 -0400

    18.08.2024

--- php_compose/php_clinica/html/application/controllers/Constructor.php
+++ php_compose/php_clinica/html/application/controllers/Constructor.php
@@ -7,8 +7,8 @@ class Constructor extends CI_Controller {
         parent::__construct();
         $this->load->helper('url');
         $this->load->library('session');
-        #$this->load->model('Modelinicio');
-        $this->load->model('Testmodel');
+        $this->load->model('Modelinicio');
+        #$this->load->model('Testmodel');
         //echo $this->Testmodel->test();
     }
 
